24a005e08099f1304c645b35eacad317fa20aff9
Chinmoy Biswas <61724571+webdev-chinmoy-biswas@users.noreply.github.com>
2022-10-15 22:32:27 +0600

    added home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,17 @@
 import React from "react";
 
-
 const Home = () => {
   return (
     <div className="home">
-      <h1>Let's Start Cooking With Popular Recipies</h1>
-      <p>
-        We created a meals recipe Web app (Food Mood) using the Meals DB API
-        With in JavaScript and React
-      </p>
-      
+      <div className="overlay">
+        <div className="home-content">
+          <h1>Let's Start Cooking With Popular Recipies</h1>
+          <p>
+            We created a meals recipe Web app (Food Mood) using the <br />Meals DB API
+            With in JavaScript and React
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
